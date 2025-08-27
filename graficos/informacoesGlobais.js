c0bbcd9de8980b8f7a5ca9cc584ca6d512695d60
mathlopes123 <142631641+mathlopes123@users.noreply.github.com>
2025-08-27 11:15:05 +0000

    vcx

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,7 +3,7 @@ const url:'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGr
 async function vizualizarInformacoesGlobais() {
 
     const resposta = await fetch(url)
-
+  
     const dados = await resposta.json()
 
     const pessoasMundo = (dados.total_pessoas_mundo/1e9)
