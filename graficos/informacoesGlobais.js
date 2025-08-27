5f5c5d74d7485f6905041deb4f86211fdc481288
mathlopes123 <142631641+mathlopes123@users.noreply.github.com>
2025-08-27 11:06:22 +0000

    de

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url:'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json'
+const url:'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json' 
 
 async function vizualizarInformacoesGlobais() {
 
@@ -13,3 +13,14 @@ async function vizualizarInformacoesGlobais() {
 }
 
 const paragrafo = document.createElement('p')
+
+paragrafo.classList.add('graficos-container__texto')
+
+paragrafo.innerHTML = `O mundo tem <span>${pessoasMundo}</span>  bilhões de pessoas, dessas pessoas, aproximadamente <span>${trabalhadoresMundo}</span>   bilhões estão empregadas e passam em média  <span>${tempoDesTrabalho}horas</span> e <span>${minutos} minutos</span> por dia no caminho para o trabalho. Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais.`
+
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo);
+
+}
+
+vizualizarInformacoesGlobais()
