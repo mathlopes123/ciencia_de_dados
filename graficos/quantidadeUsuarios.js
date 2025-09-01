f7f9a14c66937f6b5680bf4252f98d4f9ce1aea7
mathlopes123 <142631641+mathlopes123@users.noreply.github.com>
2025-09-01 10:29:23 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,1 +1,23 @@
+async function quantidadeUsuarios() {
+        const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-tipos-de-ocupacao.json'
+                const res = await fetch(url)
+                const dados = await res.json()
+                const nomeDosPostos = Object.keys(dados)
+            const quantidadeTrabalhadores = Object.values(dados)
+            
 
+                const data = [
+                {
+                x: nomeDosPostos,
+                y:quantidadeTrabalhadores,
+                type: 'bar'
+    }
+ ]
+
+ const grafico = document.createElement('div')
+ grafico.className = 'grafico'
+ document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
+}
+
+quantidadeUsuarios()
